a505276e6d72be4d7c1dcb0b797002a3184042d5
Mateo drl <maderilo@gmail.com>
2014-05-10 16:40:03 +0200

    k02-Creation of Myclass
    Completed todo0 and todo2. Pending last test.

--- CSharpKatas/K02-ExtensionMethods.cs
+++ CSharpKatas/K02-ExtensionMethods.cs
@@ -1,53 +1,62 @@
-﻿//using System.Collections.Generic;
-//using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
-//namespace CSharpKatas
-//{
-//    // todo #0: Uncomment everything (Ctrl-A, Ctrl-KU)
-//    // todo #1: Without modifying SomeoneElsesClass add a new class that makes the unit tests pass
-//    // todo #2: Refactor so that there are no loops or if statements.
-//    // todo #3: (see below)
+namespace CSharpKatas
+{
+    // todo #0: Uncomment everything (Ctrl-A, Ctrl-KU)
+    // todo #1: Without modifying SomeoneElsesClass add a new class that makes the unit tests pass
+    // todo #2: Refactor so that there are no loops or if statements.
+    // todo #3: (see below)
 
 
-//    #region DoNotModify
-//    // DO NOT MODIFY THIS FILE FOR THIS CLASS FOR THIS KATA
-//    public class SomeoneElsesClass
-//    {
-//        public List<int> Numbers { get; set; }
-//    }
-//    #endregion
+    #region DoNotModify
+    // DO NOT MODIFY THIS FILE FOR THIS CLASS FOR THIS KATA
+    public class SomeoneElsesClass
+    {
+        public List<int> Numbers { get; set; }
+    }
+    #endregion
 
-//    [TestFixture]
-//    public class TestExtensionMethods
-//    {
-//        [Test]
-//        public void FindNumberOrDefault_NumberExists_ReturnIt()
-//        {
-//            var someoneElsesClass = new SomeoneElsesClass
-//            {
-//                Numbers = new List<int> { 1, 2, 3 }
-//            };
-//            Assert.AreEqual(1, someoneElsesClass.FindNumberOrDefault(1));
-//        }
+    public class MyClass :  SomeoneElsesClass
+    {
+        public int? FindNumberOrDefault(int number)
+        {
+            return Numbers.Contains(number) ? (int?)number : null;
+        }
+    }
 
-//        [Test]
-//        public void FindNumberOrDefault_NumberDoesNotExist_ReturnNull()
-//        {
-//            var someoneElsesClass = new SomeoneElsesClass
-//            {
-//                Numbers = new List<int> { 1, 2, 3 }
-//            };
-//            var v = someoneElsesClass.FindNumberOrDefault(4);
-//            Assert.IsNull(v);
-//        }
+    [TestFixture]
+    public class TestExtensionMethods
+    {
+        [Test]
+        public void FindNumberOrDefault_NumberExists_ReturnIt()
+        {
+            var myClassInstance = new MyClass
+            {
+                Numbers = new List<int> { 1, 2, 3 }
+            };
+            Assert.AreEqual(1, myClassInstance.FindNumberOrDefault(1));
+        }
 
-//        [Test]
-//        public void FindNumberOrDefault_SomeoneElsesClassIsNull_CheckForNull()
-//        {
-//            // todo #3: What's bizarre about the following test? Why is there no error? Consider reverse engineering the code with a tool like Just Decompile to get the answer.
-//            SomeoneElsesClass someoneElsesClass = null;
-//            Assert.IsNull(someoneElsesClass.FindNumberOrDefault(4));
-//        }
-//    }
-//}
+        [Test]
+        public void FindNumberOrDefault_NumberDoesNotExist_ReturnNull()
+        {
+            var myClassInstance = new MyClass
+            {
+                Numbers = new List<int> { 1, 2, 3 }
+            };
+            var v = myClassInstance.FindNumberOrDefault(4);
+            Assert.IsNull(v);
+        }
+
+        [Test]
+        [Ignore]
+        public void FindNumberOrDefault_SomeoneElsesClassIsNull_CheckForNull()
+        {
+            // todo #3: What's bizarre about the following test? Why is there no error? Consider reverse engineering the code with a tool like Just Decompile to get the answer.
+            MyClass myClassInstance = null;
+            Assert.IsNull(myClassInstance.FindNumberOrDefault(4));
+        }
+    }
+}
 
